ba34cb5b6a9f05e77626142e3a8ccc9330c1b522
Constantin GALBENU <gica.galbenu@gmail.com>
2017-02-16 10:00:50 +0200

    added trait ObjectIdUtils

--- src/Gica/MongoDB/Lib/Traits/ObjectIdUtils.php
+++ src/Gica/MongoDB/Lib/Traits/ObjectIdUtils.php
@@ -3,7 +3,7 @@
  * Copyright (c) 2016 Constantin Galbenu <gica.galbenu@gmail.com>             *
  ******************************************************************************/
 
-namespace Gica\Cqrs\ReadModel\Traits;
+namespace Gica\MongoDB\Lib\Traits;
 
 
 use Gica\Cqrs\Event\MetaData;
